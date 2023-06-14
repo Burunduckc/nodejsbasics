32a68e5d8f58684022e1cd2c641bb331879e680e
Burunduckc <1999Melkymov@gmail.com>
2023-06-14 21:19:40 +0300

    hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,9 @@
-const calculateHash = async () => {
-    // Write your code here 
+import * as fs from 'node:fs'
+import * as crypto from "crypto";
+const calculateHash = async (filePath) => {
+    const filedata = fs.readFileSync(filePath)
+    const hash = crypto.createHash('sha256').update(filedata).digest('hex')
+    console.log(`SHA256 hash for file ${filePath}: ${hash}`)
 };
 
-await calculateHash();
+await calculateHash('src/hash/calcHash.js');
