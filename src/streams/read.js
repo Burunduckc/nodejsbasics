e566cf58b98824c6d27ba0ca2978352d81bbe73f
Burunduckc <1999Melkymov@gmail.com>
2023-06-17 00:58:08 +0300

    streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
 import * as fs from 'node:fs'
-const read = async () => {
+const read = async (urlToFile) => {
+    const readbleStream = fs.createReadStream(urlToFile, 'utf-8')
+
+    readbleStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    })
+    readbleStream.on('end', () => {
+        console.log('. File read: Operation successful')
+    })
+    readbleStream.on('error', () => {
+        throw new Error('Operation failed: missing file or uncorrect url to file')
+    })
 };
 
-await read();
+await read('src/streams/files/fileToRead.txt');
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
-const write = async () => {
-    // Write your code here 
+import * as fs from "node:fs";
+
+const write = async (url, str) => {
+    const writeFile = fs.createWriteStream(url, 'utf-8')
+    writeFile.write(str)
+    writeFile.end()
+    process.stdin.pipe(writeFile)
+    writeFile.on('finish', () => {
+        console.log('Data written to file')
+    })
+    writeFile.on('error', () => {
+        console.log('Error: file no written')
+    })
 };
 
-await write();
+await write('src/streams/files/fileToWrite.txt', 'Hello!');
