aa5216c7023a65e8b4b67edd0a38e00d7907e235
Burunduckc <1999Melkymov@gmail.com>
2023-06-17 23:29:19 +0300

    zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
-const compress = async () => {
-    // Write your code here 
+import * as zlib from 'node:zlib'
+import * as fs from 'node:fs'
+
+const compress = async (urlToFile, urlLocateZip) => {
+    const gzip = zlib.createGzip();
+    const readMyFile = fs.createReadStream(urlToFile);
+    const writeMyArchive = fs.createWriteStream(urlLocateZip);
+    readMyFile.pipe(gzip).pipe(writeMyArchive);
+    readMyFile.on('error', (err) => {
+        throw new Error('miss file')
+    })
 };
 
-await compress();
+await compress('src/zip/files/fileToCompress.txt', 'src/zip/archive.txt.gz');
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
-const decompress = async () => {
-    // Write your code here 
+import * as zlib from 'node:zlib'
+import * as fs from 'node:fs'
+//Function
+const decompress = async (urlToMyArchive, urlToMyFile) => {
+    //Streams
+    const myArchive = fs.createReadStream(urlToMyArchive)
+    const output = fs.createWriteStream(urlToMyFile)
+    //Zlib
+    const unzip = zlib.createGunzip()
+    //Pipe
+    myArchive.pipe(unzip).pipe(output)
+    myArchive.on('error', () => {
+        throw new Error('miss archive')
+    })
+    output.on('finish', () => {
+        console.log('All is okay')
+    })
 };
 
-await decompress();
+await decompress('src/zip/archive.txt.gz', 'src/zip/files/fileToCompress.txt');
